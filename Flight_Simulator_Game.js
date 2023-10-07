e9f1910ccd927af978e00826672ed06e20d52568
CrazyBeastable <shrivedpatade@gmail.com>
2023-10-07 14:57:59 +0530

    Multiple Player system added

--- Flight_Simulator_Game.js
+++ Flight_Simulator_Game.js
@@ -1,19 +1,16 @@
 let clouds = [];
-let score = 0;
-let isJumping = 0;
-let frameShootRate = 0;
 let framebuildingSpawnRate = 0;
 let buildings = []; 
+let players = [];
 
 function setup() {
     createCanvas(720,420);
 
     reset();
-
 }
 
-function setClouds(){
-    for(let i = 0; i<5; i++){
+function setClouds(num=5){
+    for(let i = 0; i<num; i++){
         let cloud = {
             no:i,
             x:random(width,width+1000),
@@ -24,19 +21,17 @@ function setClouds(){
 }
 
 function reset(){
-    score = 0;
-    isJumping = 0;
-    frameShootRate = 0;
+    player = [];
     framebuildingSpawnRate = 0;
     buildings = [];
     clouds = [];
     playerSprite = loadImage('Assets/Plane.png');
-    playerHeight = height/2;
     buildingLowerSprite = loadImage('Assets/Building(Cropped).png');
     buildingUpperSprite = loadImage('Assets/Building(Cropped)(HorizontalFlipped).png');
     backgroundSprite = loadImage('Assets/sky background.jpg');
     cloudSprites = [loadImage('Assets/cloud1.jpg'),loadImage('Assets/cloud2.jpg'),loadImage('Assets/cloud3.jpg'),loadImage('Assets/cloud4.jpg'),loadImage('Assets/cloud5.jpg')];
-    setClouds();
+    setClouds();    //pass the number of clouds to print (default = 5)
+    createPlayers();    //pass the number of players (default = 1)
     frameRate(60);
     rectMode(CENTER);
 }
@@ -50,7 +45,7 @@ function draw() {
         cloud.x -= 5*(deltaTime/40);
 
         //render cloud
-        image(cloudSprites[cloud.no],cloud.x+cloud.no*10,cloud.y,width/5,height/5);
+        image(cloudSprites[cloud.no%5],cloud.x+cloud.no*10,cloud.y,width/5,height/5);
         if(cloud.x<-250){
             cloud.x = random(width+50,width+200);
             cloud.y = random(50,height-50);
@@ -80,48 +75,60 @@ function draw() {
         }
     }
 
-    //check for collisions
-    for(let building of buildings){
-        if(dist(width/5,playerHeight,building.upperStrikeZoneX,building.upperStrikeZoneY)<85||dist(width/5,playerHeight,building.lowerStrikeZoneX,building.lowerStrikeZoneY)<100){
-            reset();
+    //load players
+    for(let player of players){
+        //set player sprite
+        image(playerSprite,width/5,player.playerHeight,25+width/15,height/15);
+
+        //jumping logic
+        if(player.isJumping != 1){
+            player.playerHeight += 2*(deltaTime/50);
         }
-    }
+        if(player.isJumping == 1){
+            player.playerHeight -= 4*(deltaTime/50);
+        }
+        if(player.frameJumpRate%60 == 0){
+            player.isJumping = 0;
+        }
+        if(keyIsDown(UP_ARROW)&&player.isJumping!=1){
+            player.isJumping = 1;
+            player.frameJumpRate = 0;
+        }
+        player.frameJumpRate++;
 
-    //load player
-    image(playerSprite,width/5,playerHeight,25+width/15,height/15);
+        //check for collisions
+        for(let building of buildings){
+            if(dist(width/5,player.playerHeight,building.upperStrikeZoneX,building.upperStrikeZoneY)<85||dist(width/5,player.playerHeight,building.lowerStrikeZoneX,building.lowerStrikeZoneY)<100){
+                players.splice(players.indexOf(player),1);
+            }
+        }
 
-    //jumping logic
-    if(isJumping != 1){
-        playerHeight += 2*(deltaTime/50);
-    }
-    if(isJumping == 1){
-        playerHeight -= 4*(deltaTime/50);
-    }
-    if(frameShootRate%60 == 0){
-        isJumping = 0;
-    }
-    if(keyIsDown(UP_ARROW)&&isJumping!=1){
-        isJumping = 1;
-        frameShootRate = 0;
-    }
-    frameShootRate++;
+        //reset game if player out of bounds
+        if(player.playerHeight<0||player.playerHeight>height){
+            players.splice(players.indexOf(player),1);
+        }
 
-    //reset game if out of bounds
-    if(playerHeight<0||playerHeight>height){
-        reset();
+        //print score
+        fill(0,0,0);
+        for(let i=0;i<players.length;i++){
+            text(player.score,5*(player.no*5+1),25);
+        }
     }
 
     //increment score
     for(let building of buildings){
-        if(width/5 > building.upperTowerX+100){
-            score+=building.scorePoint;
-            building.scorePoint = 0;
+        for(let player of players){
+            if(width/5 > building.upperTowerX+100&&building.scorePoint!=0){
+                player.score+=1;
+                building.scorePoint--;
+            }
         }
     }
 
-    //print score
-    fill(0,0,0);
-    text(score,15,25);//remaining
+    //reset is all players are dead
+    if(players.length==0){
+        reset();
+    }
 }
 
 function createbuilding(){
@@ -135,7 +142,21 @@ function createbuilding(){
         upperStrikeZoneY:y-750+775-width/5,
         lowerStrikeZoneX:width+172,
         lowerStrikeZoneY:y+125+230-width/5,
-        scorePoint:1
+        scorePoint:players.length
     }
     buildings.push(building);
+}
+
+function createPlayers(num = 1){
+    for(let i=0;i<num;i++)
+    {
+        let player = {
+            no:i,
+            score:0,
+            playerHeight:random(100,height-100),
+            isJumping:0,
+            frameJumpRate:0
+        }
+        players.push(player);
+    }
 }
